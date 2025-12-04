a92dd9967d9cf32e63f057eeafccfc7179fd4eb7
tydeargarin <argarintyde23@gmail.com>
2025-12-04 19:53:26 +0800

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -215,6 +215,8 @@ def api_analyze() -> object:
 if __name__ == "__main__":
     # Start Flask development server
     # Access the app via http://127.0.0.1:5000/ in your browser
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
